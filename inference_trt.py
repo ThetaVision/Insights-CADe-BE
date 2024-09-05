eb76d2247a9fb28faf0c3afbf9f895a4e670edc4
Koen Kusters <c.h.j.kusters@tue.nl>
2024-09-05 15:55:41 +0200

    Initial Commit

--- inference_trt.py
+++ inference_trt.py
@@ -591,7 +591,7 @@ def run(opt, f_txt, exp_name, inf_set):
             # Create original image with heatmap overlay
             composite = Image.blend(heatmap_pil, image, 0.6)
             draw = ImageDraw.Draw(composite)
-            font = ImageFont.truetype('C:/Users/s157128/Documents/Roboto/Roboto-Regular.ttf', size=48)
+            font = ImageFont.truetype('arial.ttf', size=48)
             draw.text(
                 (0, 0),
                 "Cls: {:.3f}, Seg:{:.3f}".format(cls_pred.item(), mask_cls_logit.item()),
